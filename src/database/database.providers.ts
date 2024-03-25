062d3494a8cace17918179fb9ecb974fba6ea31b
André Comegno <55299009+andrecomegno@users.noreply.github.com>
2024-03-25 19:52:05 -0300

    Update database.providers.ts

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -9,7 +9,7 @@ export const databaseProviders = [
         host: 'localhost',
         port: 3306,
         username: 'root',
-        password: '123456',
+        password: '',
         database: 'projetoapi',
         entities: [
             __dirname + '/../**/*.entity{.ts,.js}',
